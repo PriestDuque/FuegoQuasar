75e4327b45ef9ae32236e3cf31e770d29786e1df
Julian Duque <julian.duquec@perficient.com>
2021-07-05 16:41:32 -0500

    Ajuste para persistencia

--- src/test/java/com/aws/quasar/handler/FuegoQuasarSplitGetHandlerTest.java
+++ src/test/java/com/aws/quasar/handler/FuegoQuasarSplitGetHandlerTest.java
@@ -1,8 +1,6 @@
 package com.aws.quasar.handler;
 
 import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext;
-import com.aws.quasar.descifrador.DescifradorException;
-import com.aws.quasar.util.ArchivoUtil;
 import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,12 +15,13 @@ class FuegoQuasarSplitGetHandlerTest {
 
     private static final String EXPECTED_CONTENT_TYPE = "application/json";
     private static final int EXPECTED_STATUS_CODE_SUCCESS = 200;
+    private static final int EXPECTED_STATUS_CODE_INDESCIFRABLE = 404;
 
     private final MockLambdaContext mockLambdaContext = new MockLambdaContext();
 
     @Test
     @DisplayName("Se obtiene el resultado de los tres satelites almacenado en los archivos")
-    void testHandleRequestCorrecto() throws DescifradorException {
+    void testHandleRequestCorrecto() {
         String input="{\"distance\":1500,\"message\":[\"este\",\"\",\"\", \"mensaje\",\"\"]}";
         new FuegoQuasarSplitKenobiHandler().handleRequest(input, mockLambdaContext);
         input="{\"distance\":632.46,\"message\":[\"este\", \"\", \"un\", \"\", \"\"]}";
@@ -38,6 +37,26 @@ class FuegoQuasarSplitGetHandlerTest {
         assertEquals("{\"position\":{\"x\":700.0,\"y\":700.0},\"message\":\"este es un mensaje secreto\"}", jsonObjectFromResponse.get("Output"));
         assertEquals(EXPECTED_CONTENT_TYPE, response.getHeaders().get("Content-Type"));
         assertEquals(EXPECTED_STATUS_CODE_SUCCESS, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Se obtiene el resultado de los tres satelites almacenado en los archivos")
+    void testHandleRequestIncorrecto() {
+        String input="{\"distance\":100,\"message\":[\"este\",\"\",\"\", \"mensaje\",\"\"]}";
+        new FuegoQuasarSplitKenobiHandler().handleRequest(input, mockLambdaContext);
+        input="{\"distance\":142.7,\"message\":[\"este\", \"\", \"un\", \"\", \"\"]}";
+        new FuegoQuasarSplitSatoHandler().handleRequest(input, mockLambdaContext);
+        input="{\"distance\":115.5,\"message\":[\"\", \"es\", \"\", \"\", \"secreto\"]}";
+        new FuegoQuasarSplitSkywalkerHandler().handleRequest(input, mockLambdaContext);
+
+        GatewayResponse response = (GatewayResponse) new FuegoQuasarSplitGetHandler().handleRequest(null, mockLambdaContext);
+
+        // Verify the response obtained matches the values we expect.
+        JSONObject jsonObjectFromResponse = new JSONObject(response.getBody());
+
+        assertEquals("Mensaje indescifrable", jsonObjectFromResponse.get("Output"));
+        assertEquals(EXPECTED_CONTENT_TYPE, response.getHeaders().get("Content-Type"));
+        assertEquals(EXPECTED_STATUS_CODE_INDESCIFRABLE, response.getStatusCode());
     }
 
 }
