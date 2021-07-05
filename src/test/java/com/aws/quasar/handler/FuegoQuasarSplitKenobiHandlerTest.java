43bb94c651c05bfe3c1512e4904fcc3a73363554
Julian Duque <julian.duquec@perficient.com>
2021-07-05 08:43:33 -0500

    Prueba persistencia dynamo

--- src/test/java/com/aws/quasar/handler/FuegoQuasarSplitKenobiHandlerTest.java
+++ src/test/java/com/aws/quasar/handler/FuegoQuasarSplitKenobiHandlerTest.java
@@ -1,4 +1,54 @@
 package com.aws.quasar.handler;
 
+import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext;
+import com.amazonaws.services.dynamodbv2.local.main.ServerRunner;
+import com.amazonaws.services.dynamodbv2.local.server.DynamoDBProxyServer;
+import org.json.JSONObject;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests for {@link FuegoQuasarSplitKenobiHandler}.
+ */
+@DisplayName("Tests for FuegoQuasarHandler")
 public class FuegoQuasarSplitKenobiHandlerTest {
+
+    private static final String EXPECTED_CONTENT_TYPE = "application/json";
+    private static final int EXPECTED_STATUS_CODE_SUCCESS = 200;
+
+    private static DynamoDBProxyServer server;
+
+    private final MockLambdaContext mockLambdaContext = new MockLambdaContext();
+
+    @BeforeClass
+    public static void setupClass() throws Exception {
+        System.setProperty("sqlite4java.library.path", "native-libs");
+        String port = "8000";
+        server = ServerRunner.createServerFromCommandLineArgs(
+                new String[]{"-inMemory", "-port", port});
+        server.start();
+    }
+
+    @AfterClass
+    public static void teardownClass() throws Exception {
+        server.stop();
+    }
+
+   // @Test
+    @DisplayName("El mensaje que ingresa se guardar en la base de datos")
+    void testHandleRequestCorrecto() {
+        String input="{\"name\":\"kenobi\",\"distance\":1500,\"message\":[\"este\", \"\", \"\", \"mensaje\", \"\"]}";
+        GatewayResponse response = (GatewayResponse) new FuegoQuasarSplitKenobiHandler().handleRequest(input, mockLambdaContext);
+
+        // Verify the response obtained matches the values we expect.
+        JSONObject jsonObjectFromResponse = new JSONObject(response.getBody());
+        assertEquals("OK", jsonObjectFromResponse.get("Output"));
+        assertEquals(EXPECTED_CONTENT_TYPE, response.getHeaders().get("Content-Type"));
+        assertEquals(EXPECTED_STATUS_CODE_SUCCESS, response.getStatusCode());
+    }
+
 }
